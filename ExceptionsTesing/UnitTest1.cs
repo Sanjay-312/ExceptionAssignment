df267e7c1adbd64118ab8582374d88c2e5b59933
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-10-03 18:34:52 +0530

    [sanjeeva] : checked for null message

--- ExceptionsTesing/UnitTest1.cs
+++ ExceptionsTesing/UnitTest1.cs
@@ -20,5 +20,21 @@ namespace ExceptionsTesing
             Assert.AreEqual(expected, actual);
             
         }
+
+        [TestMethod]
+        public void given_null_should_return_happy()
+        {
+            //analyse
+            string expected = "HAPPY";
+            string message = null;
+            MoodAnalyser analyser = new MoodAnalyser(message);
+
+            //act
+            string actual = analyser.analyse_mood();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
     }
 }
